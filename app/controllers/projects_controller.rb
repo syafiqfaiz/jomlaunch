a189e45fa3649a11c981ed0118d3a42ecf4f1c00
syafiq faiz <faiz@syafiqfaizs-MacBook-Air.local>
2015-02-24 14:50:17 +0800

    set user bug

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -69,7 +69,11 @@ class ProjectsController < ApplicationController
     end
 
     def set_user
-      @user = User.find(session[:user_id])
+      if session[:user_id] == nil 
+        redirect_to '/login'
+      else
+        @user = User.find(session[:user_id])
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
