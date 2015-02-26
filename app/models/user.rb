c0a4ad81102e15d643ca1e6b8da60ddb7f164903
syafiq faiz <faiz@syafiqfaizs-MacBook-Air.local>
2015-02-26 15:55:23 +0800

    github login

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,21 @@ class User < ActiveRecord::Base
 	devise :omniauthable, :omniauth_providers => [:github]
   has_many :projects
 
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["name"]
+    end
+  end
+
+  def self.from_omniauth(auth)
+  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0,20]
+    user.name = auth.info.name   # assuming the user model has a name
+    user.image = auth.info.image # assuming the user model has an image
+  end
+end
 end
