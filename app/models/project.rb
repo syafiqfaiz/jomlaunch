3f21c9138f2b2aae055158e2448ead008bb50a92
syafiq faiz <faiz@syafiqfaizs-MacBook-Air.local>
2015-03-03 11:54:54 +0800

    skip before authentication

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ActiveRecord::Base
 	belongs_to :user
+	validates :project_name, :brief_description, :description, :web_link, presence: true
+	validates :web_link, uniqueness: true
 end
