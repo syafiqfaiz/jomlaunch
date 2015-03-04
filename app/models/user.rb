25e90121d6843f2c0c87aabffe425f8911e3adc8
syafiq faiz <faiz@syafiqfaizs-MacBook-Air.local>
2015-03-04 17:30:47 +0800

    disable github login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:github]
+         :recoverable, :rememberable, :trackable, :validatable#, :omniauthable, :omniauth_providers => [:github]
   has_many :projects
 	validates :email, :encrypted_password, presence: true
 	validates :email, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users
   resources :projects
 
   # get '/login', to: 'users#login'  
